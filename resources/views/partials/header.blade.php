f3d638f6198a1768d1895575811eea597b72082b
Lorenzo Magro <magrolorenzo@gmail.com>
2020-12-22 17:22:12 +0100

    Stilato header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,26 +1,29 @@
 <header>
-    <div class="">
-        <img src="{{asset('images/marchio-sito-test.png')}}" alt="">
-    </div>
-    <div class="">
-        <nav>
-            <ul>
-                <li>
-                    <a href="{{ url('/')}}">
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('pagina-prodotti') }}">
-                        Prodotti
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('news') }}">
-                        News
-                    </a>
-                </li>
-            </ul>
-        </nav>
+    <div class="container">
+
+        <div class="logo-container">
+            <img src="{{asset('images/marchio-sito-test.png')}}" alt="">
+        </div>
+        <div class="navbar-container">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="{{ url('/')}}">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('pagina-prodotti') }}">
+                            Prodotti
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('news') }}">
+                            News
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </header>
